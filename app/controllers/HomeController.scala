294c6cd64e8ba87738b52c7df240cf59c612cc6b
Danny <dannypen@gmail.com>
2020-03-18 11:35:30 +0000

    Homepage now shows which user you're logged in as

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -6,8 +6,8 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index: Action[AnyContent] = Action {implicit request:Request[AnyContent] =>
+    Ok(views.html.index("Logged in as: " + request.session.data("user")))
   }
 
 }
