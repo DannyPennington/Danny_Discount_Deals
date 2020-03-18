9c71f4f63801d559f8f58d304aa85219755252a3
Danny <dannypen@gmail.com>
2020-03-18 16:25:02 +0000

    Whitespace removed

--- app/models/Registration.scala
+++ app/models/Registration.scala
@@ -1,12 +1,10 @@
 package models
-
 import play.api.data.Form
 import play.api.data.Forms._
 
 case class Registration(forename:String, surname:String, email: String, password: String)
 
 object Registration {
-
   val RegistrationForm:Form[Registration] = Form(
     mapping(
       "forename" -> nonEmptyText,
