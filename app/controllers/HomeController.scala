e26a8b56bec46baab71ee3892daefe7397b821f0
Danny <dannypen@gmail.com>
2020-03-19 09:28:56 +0000

    Fixed error if no one is logged in

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,7 +7,12 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index: Action[AnyContent] = Action {implicit request:Request[AnyContent] =>
-    Ok(views.html.index("Logged in as: " + request.session.data("user")))
+    if (request.session.get("user").isDefined) {
+      Ok(views.html.index("Logged in as: " + request.session.data("user")))
+    }
+    else {
+      Ok(views.html.index("Welcome to the site!"))
+    }
   }
 
 }
